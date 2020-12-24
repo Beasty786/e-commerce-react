2d7c812a1587878b658c321b6ece60329e416a8b
beasty786 <shameelnkosi@gmail.com>
2020-12-24 16:33:04 +0200

    added user action types for consistence

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch , Route } from 'react-router-dom';
+import { Switch , Route , Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 // pages
@@ -54,7 +54,15 @@ class App extends React.Component{
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact
+          path = '/signin' 
+          render = { () => 
+            this.props.currenUser != null ?
+            (<Redirect to='/shop'/>)
+            :
+            <SignInAndSignUpPage />
+          }
+        />
       </Switch>
       
     </div>
@@ -62,8 +70,12 @@ class App extends React.Component{
   }
 }
 
+const mapStateToProps = ({ user }) =>({
+  currenUser: user.currenUser
+})
+
 const mapDispatchToProps = dispatch =>({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null ,mapDispatchToProps )(App);
+export default connect(mapStateToProps ,mapDispatchToProps )(App);
