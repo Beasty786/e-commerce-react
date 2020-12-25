e711808814b185dc5e9321736de41d49cc079f30
beasty786 <shameelnkosi@gmail.com>
2020-12-25 02:41:34 +0200

    added the icon component as well as the cart dropdown

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-
 import { connect } from 'react-redux';
 
 import { ReactComponent as Logo} from '../../assets/crown.svg';
-
 import './header.styles.scss';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser , hidden }) => (
     <div className = 'header'>
         <Link to="/" className = 'logo-container'>
             <Logo className = 'logo'/>
@@ -25,7 +25,6 @@ const Header = ({ currentUser }) => (
             </Link>
             
             {
-                
                 currentUser  ?
                 <div className = 'option' onClick = {() => { console.log('signing out'); auth.signOut();}}>SIGN OUT</div> 
                 :
@@ -34,13 +33,20 @@ const Header = ({ currentUser }) => (
                 </Link>
             }
 
+            <CartIcon/>
            
         </div>
+
+        {
+            hidden ? null : <CartDropdown/>   
+        }
+        
     </div>
 )
 
-const  mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const  mapStateToProps = ({user: {currentUser} , cart : {hidden}}) => ({
+    currentUser ,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -1,10 +1,9 @@
-// Represents all the reducers
-
-
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer'
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
